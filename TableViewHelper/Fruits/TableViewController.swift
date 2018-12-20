58c4fc0c35f8e8b5d7d47e9d8ba9b72fb5d87d00
ranjit <ranjithkumar2010a@live.com>
2018-12-21 01:22:15 +0530

    code cleanup

--- TableViewHelper/Fruits/TableViewController.swift
+++ TableViewHelper/Fruits/TableViewController.swift
@@ -31,3 +31,19 @@ final class TableViewController: UITableViewController {
         tableView.reloadData()
     }
 }
+
+
+//(i)Simply we are populating the TableView with Strings
+//    var array: [String] = ["Apple","Orange","Pineapple","Grapes","Gova"]
+//    typealias T = String
+
+////(ii)Populating the Model Data in TableView
+//typealias T = Fruit
+//    var array: [Fruit] = [
+//        Fruit(name: "Apple"),
+//        Fruit(name: "Orange"),
+//        Fruit(name: "Pineapple"),
+//        Fruit(name: "Grapes"),
+//        Fruit(name: "Gova")]
+
+//(iii)Here we have Fruits Model and it has one array and it contains list of fruits and we are populating this on TableView
--- TableViewHelper/Fruits/TableViewModel.swift
+++ TableViewHelper/Fruits/TableViewModel.swift
@@ -1,26 +1,8 @@
 import Foundation
 import UIKit
 
-final class TableViewModel: NSObject {
-    //(i)Simply we are populating the TableView with Strings
-    //    var array: [String] = ["Apple","Orange","Pineapple","Grapes","Gova"]
-    //    typealias T = String
-
-    ////(ii)Populating the Model Data in TableView
-    //typealias T = Fruit
-    //    var array: [Fruit] = [
-    //        Fruit(name: "Apple"),
-    //        Fruit(name: "Orange"),
-    //        Fruit(name: "Pineapple"),
-    //        Fruit(name: "Grapes"),
-    //        Fruit(name: "Gova")]
-
-    //(iii)Here we have Fruits Model and it has one array and it contains list of fruits and we are populating this on TableView
+struct TableViewModel {
     let fruits: Fruits
-
-    init(fruits: Fruits) {
-        self.fruits = fruits
-    }
 }
 
 //MARK: - TableViewDataSource methods
